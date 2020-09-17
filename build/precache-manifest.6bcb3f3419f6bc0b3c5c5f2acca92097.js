8d72da8a1397e7a391b112b96b683f654fdf5c5e
Tester <mirzajahangeer@gmail.com>
2020-09-17 19:26:50 +0500

    build

---
+++ build/precache-manifest.6bcb3f3419f6bc0b3c5c5f2acca92097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff991ec42ab654a7f59818536707a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c879def7f9d90bd7e5c1",
+    "url": "/static/css/main.dea09002.chunk.css"
+  },
+  {
+    "revision": "906df0701e596d620b23",
+    "url": "/static/js/2.061d05ed.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.061d05ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c879def7f9d90bd7e5c1",
+    "url": "/static/js/main.ba1725c7.chunk.js"
+  },
+  {
+    "revision": "8862016860622b01e6aa",
+    "url": "/static/js/runtime-main.f22b2772.js"
+  }
+]);
